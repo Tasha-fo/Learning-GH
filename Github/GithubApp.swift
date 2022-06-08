84ab67a40bc2394c6fbedc403647a877013ec6ea
Natasha Foley <Tashafoley7@gmail.com>
2022-06-08 19:37:29 -0400

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Natasha Foley on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
